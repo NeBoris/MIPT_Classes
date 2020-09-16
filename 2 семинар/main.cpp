96e4825f8fd501ccce8121ebb4d7a5f97605cce0
NeBoris <69312516+NeBoris@users.noreply.github.com>
2020-09-16 18:49:16 +0300

    English version

--- 2 семинар/main.cpp
+++ 2 семинар/main.cpp
@@ -2,10 +2,13 @@
 #include <vector>
 #include <string>
 #include <iomanip>
+#include <cmath>
 //#include <sstream>
 #include <stdexcept>
 
-void Print(const std::vector<std::vector<double>> &matrix){
+
+//rint - print matrix
+void print(const std::vector<std::vector<double>> &matrix){
 	for(const auto &g : matrix){
 		for(const auto &c : g){
 			std::cout << std::setw(5) << std::right;
@@ -19,13 +22,45 @@ void Print(const std::vector<std::vector<double>> &matrix){
 	}
 }
 
-void Divide(std::vector<double> & vec, double n){
+
+//print_solution - print partial solution and fundamental matrix
+void print_solution(const std::vector<std::vector<double>> &matrix){
+	std::cout << "Partial solution: \n";
+	for(const auto &g : matrix){
+		std::cout << std::setw(5) << std::right;
+		if(g[int(g.size()) - 1] == -0){
+			std::cout << 0;
+		}else{
+			std::cout << g[int(g.size()) - 1];
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << "\nFundamental matrix:\n";
+
+	for(const auto &g : matrix){
+		for(auto i = 0; i < int(g.size()) - 1; ++i){
+			std::cout << std::setw(5) << std::right;
+			if(g[i] == -0){
+				std::cout << 0 << " ";
+			}else{
+				std::cout << g[i] << " ";
+			}
+		}
+		std::cout << '\n';
+	}
+}
+
+//divide - divide row (vector) by number n
+void divide(std::vector<double> & vec, double n){
 	for(auto &c : vec){
 		c /= n;
 	}
 }
 
-void Clear_column(std::vector<std::vector<double>> &matrix, const int &row, const int &column){
+//clear_column - subtracts the given row from each
+//				 row multiplied by the corresponding coefficient
+void clear_column(std::vector<std::vector<double>> &matrix, const int &row, const int &column){
 	int temp;
 
 	for(int i = 0; i < int(matrix.size()); ++i){
@@ -39,6 +74,8 @@ void Clear_column(std::vector<std::vector<double>> &matrix, const int &row, cons
 	}
 }
 
+//solution - returns matrix with the last column - partial solution
+//			 the rest columns are columns of fundamental matrix
 std::vector<std::vector<double>> solution(const std::vector<std::vector<double>> &matrix){
 	std::vector<std::vector<double>> fundam;
 	std::vector<double> temp;
@@ -50,28 +87,41 @@ std::vector<std::vector<double>> solution(const std::vector<std::vector<double>>
 			break;
 		}
 	}
+	if(row == int(matrix.size())){
+		--row;
+	}
 
-	if(row == column && row == int(matrix.size())){
-		throw std::invalid_argument("Система несовместна");
+	//print(matrix);
+	//std::cout << "\n" << row << " " << column << std::endl;
+	//check for incorrect system
+	for(int i = row; i < int(matrix.size()); ++i){
+		if(matrix[i][int(matrix[0].size()) - 1] != 0){
+			throw std::invalid_argument("The system is incompatible");
+		}
+	}
+
+	if(column == int(matrix[0].size())){
+		--column;
 	}
 
 	for(int j = 0; j < row; ++j){
 		for(int i = column; i < int(matrix[0].size()); ++i){
-			temp.push_back(-matrix[j][i]);
+			if(i == int(matrix[0].size()) - 1){
+				temp.push_back(matrix[j][i]);
+			}else{
+				temp.push_back(-matrix[j][i]);
+			}
 		}
 		fundam.push_back(temp);
 		temp.clear();
 	}
 
-	//check for incorrect system
-	for(int i = row; i < int(matrix.size()); ++i){
-		if(matrix[i][int(matrix[0].size()) - 1] != 0){
-			throw std::invalid_argument("Система несовместна");
-		}
-	}
-
 	for(int j = 0; j < int(matrix.size()) - row; ++j){
 		for(int i = 0; i < int(matrix[0].size()) - column; ++i){
+			if(i == int(matrix[0].size()) - column - 1){
+				temp.push_back(0);
+				continue;
+			}
 			if(i == j){
 				temp.push_back(1);
 			}else{
@@ -83,7 +133,6 @@ std::vector<std::vector<double>> solution(const std::vector<std::vector<double>>
 	}
 
 
-
 	return fundam;
 }
 
@@ -91,37 +140,46 @@ int main(){
 	std::vector<std::vector<double>> matrix;
 	std::vector<double> temp;
 	double c;
-	int N = 3, flag = 1;
+	int N = 3, flag = 1;					//flag is used to change rows if dioganal element is 0
 	std::stringstream s("");
 
 
-	s << "1" << ' ' << "2" << ' ' << "3" << ' ' << "-1" << '\n'
+	 /*     s << "1" << ' ' << "2" << ' ' << "3" << ' ' << "-1" << '\n'
 			<< "2" << ' ' << "3" << ' ' << "4" << ' ' << "1" << '\n'
 			<< "1" << ' ' << "1" << ' ' << "1" << ' ' << "2" << '\n';
+	//		<< "1" << ' ' << "0" << ' ' << "0" << ' ' << "1" << '\n';*/
+	      s << "6" << ' ' << "-1" << ' ' << "1" << ' ' << "-1" << '\n'
+			<< "5" << ' ' << "-5" << ' ' << "5" << ' ' << "0" << '\n'
+			<< "4" << ' ' << "-9" << ' ' << "9" << ' ' << "1" << '\n';
 	//		<< "1" << ' ' << "0" << ' ' << "0" << ' ' << "1" << '\n';
 
+	/*	  s << "1" << ' ' << "0" << ' ' << "0" << ' ' << "0" << '\n'
+			<< "0" << ' ' << "1" << ' ' << "0" << ' ' << "0" << '\n'
+			<< "0" << ' ' << "0" << ' ' << "1" << ' ' << "0" << '\n'
+			<< "0" << ' ' << "0" << ' ' << "0" << ' ' << "1" << '\n';*/
 
 
-	std::cout << "Введите число уравнений в системе: ";
+
+	std::cout << "Enter the number of equations in the system: ";
 	//std::cin >> N;
 
 
-	std::cout << "Введите коэффициенты системы уравнений в виде матрицы (нажмите q для справки): " << std::endl;
+	std::cout << "Enter coefficients (press q for help): " << std::endl;
 
 
-	std::cin.ignore(1);
+	//std::cin.ignore(1);
 	//if(std::cin.peek() == 'q'){
 	if(s.peek() == 'q'){
-		std::cout << "Правила ввода:\n"
-				<< "1. Коэффициенты одного уравнения вводятся в одной строке через пробел\n"
-				<< "2. Нулевые коэффициенты обязательны для ввода\n"
-				<< "3. При переходе к коэффициентам следующего уравнения нажать 'enter' " << std::endl;
-		std::cout << "Введите коэффициенты: " << std::endl;
+		std::cout << "Rules:\n"
+				<< "1. Enter the coefficients of a single equation in a single line separated by a space\n"
+				<< "2. Zero coefficients are required for entering\n"
+				<< "3. When going to the coefficients of the next equation, click 'enter' " << std::endl;
+		std::cout << "Enter the coefficients: " << std::endl;
 		//std::cin.ignore(1);
 		s.ignore(1);
 	}
 
-
+	//fill the matrix whith coefficients
 	for(int i = 0; i < N; i++){
 		//if(std::cin.peek() == 10){
 		if(s.peek() == 10){
@@ -142,33 +200,36 @@ int main(){
 	row < int(matrix.size()); ++column,++row){
 
 			//std::cout << row << "-" << column << " '" << matrix[row][column] << "'" << std::endl;
-			if(matrix[row][column] == 0)
+			if(matrix[row][column] == 0)	//swap the rows if dioganal element in current one is 0
 			{
 				if(row != int(matrix.size()) - flag){
 					std::swap(matrix[row], matrix[row+flag]);
 					--row;
 					--column;
 					++flag;
-					//Print(matrix);
+					//print(matrix);
 				}else{
 					break;
 				}
 			}else
 			{
-				Divide(matrix[row], matrix[row][column]);
+				//make this diagonal element 1 (and divide other elements in row)
+				divide(matrix[row], matrix[row][column]);
 				//Print(matrix);
-				Clear_column(matrix, row, column);
-				//Print(matrix);
+				//make other elements in column 0
+				clear_column(matrix, row, column);
+				print(matrix);
 			}
 
 
 	}
 
+	//Print(matrix);
+
 	try{
-		Print(solution(matrix));
-		std::cout << "Фундаментальная матрица для данной системы уравнений. Её столбцы являются базисными векторами пространства решений." << std::endl;
+		print_solution(solution(matrix));
 	}catch(const std::invalid_argument &ia){
-		std::cout << "Invalid argument: " << ia.what() << std::endl;;
+		std::cout << "Error! " << ia.what() << std::endl;;
 	}
 	return EXIT_SUCCESS;
 }
