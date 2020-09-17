5944bf372db10f58c3c77f593a64179b5dc0bc43
NeBoris <69312516+NeBoris@users.noreply.github.com>
2020-09-17 16:11:08 +0300

    without pointers

--- 2 семинар/main.cpp
+++ 2 семинар/main.cpp
@@ -31,10 +31,10 @@ bool is_equal(double lhs, double rhs){
 }
 
 //to check user's input and ask him to write correct data
-void try_again(int* N, int* M){
+void try_again(int N, int M){
 	std::cout << "\nAmount of equations in a system must be less or equal to number of variables.\nTry again: ";
-	std::cin >> *N >> *M;
-	if(*N > *M){
+	std::cin >> N >> M;
+	if(N > M){
 		try_again(N, M);
 	}
 }
@@ -44,33 +44,33 @@ void try_again(int* N, int* M){
 //size_e - width (size of equation, row in matrix)
 //zero_columns - vector with numbers of matrix's columns in which there are no diagonal element even after swaping the rows
 void get_solution(const std::vector< std::vector<double>> &matrix, const std::vector<unsigned int> &zero_columns,
-		unsigned int* size_c, unsigned int* size_e){
+		unsigned int size_c, unsigned int size_e){
 
 
-	std::vector< std::vector<double>> fundamental_matrix(*size_e - 1);
-	std::vector<double> partial_sol(*size_e - 1, 0.0);
+	std::vector< std::vector<double>> fundamental_matrix(size_e - 1);
+	std::vector<double> partial_sol(size_e - 1, 0.0);
 
 	//if there is identity matrix with its height equal to the height of the original matrix 
 	if(zero_columns.empty()){
 
-		for(auto i = 0U; i < *size_c; ++i){
+		for(auto i = 0U; i < size_c; ++i){
 			partial_sol[i] = matrix[i].back();
 		}
 
-		if(*size_c < *size_e - 1){
-			for(auto i = 0U; i < *size_e - 1; ++i){
-				for(auto j = *size_c; j < *size_e - 1 && i < *size_c; ++j){
+		if(size_c < size_e - 1){
+			for(auto i = 0U; i < size_e - 1; ++i){
+				for(auto j = size_c; j < size_e - 1 && i < size_c; ++j){
 					fundamental_matrix[i].push_back(-matrix[i][j]);
 				}
-				for(auto j = 0U; (j < *size_e - *size_c - 1)&& i >= *size_c; ++j){
-					(i - *size_c) == j ?  fundamental_matrix[i].push_back(1.0) : fundamental_matrix[i].push_back(0.0);
+				for(auto j = 0U; (j < size_e - size_c - 1)&& i >= size_c; ++j){
+					(i - size_c) == j ?  fundamental_matrix[i].push_back(1.0) : fundamental_matrix[i].push_back(0.0);
 				}
 			}
 
 		}
 	//if there are matrix's columns in which there are no diagonal element even after swaping the rows
 	}else{
-		for(auto i = 0U, k = 0U; k < *size_c && (i < *size_e - 1); ++i, ++k){
+		for(auto i = 0U, k = 0U; k < size_c && (i < size_e - 1); ++i, ++k){
 			if(find_in_vector(zero_columns,i) == -1){
 				partial_sol[i] = matrix[k].back();
 				for(auto c : zero_columns){
@@ -97,52 +97,49 @@ void get_solution(const std::vector< std::vector<double>> &matrix, const std::ve
 }
 
 int main(){
-	double* temp = new double;
-	int* N = new int(4); //amount of equations
-	int* M = new int(5); //amount of coefficients
+	double temp;
+	int N; //amount of equations
+	int M; //amount of coefficients
 	//std::stringstream s("");
 	unsigned int row = 0U;
 
 	std::cout << "Enter amount of equations in a system and number of coefficients: ";
-	std::cin >> *N >> *M;
-	if(*N > *M){
+	std::cin >> N >> M;
+	if(N > M){
 		try_again(N, M);
 	}
 
 	//s << 1 << 2 << 3 << 4 << 2 << 5 << 17 << 0 << 3 << -2 << 8 << 1;
 	//s << "1 0 0 3 1 0 1 0 4 2 0 0 1 5 3 0 0 0 0 4";
 
-	std::vector< std::vector<double> > matrix(*N);
+	std::vector< std::vector<double> > matrix(N);
 
 	std::cout << "Enter coefficients: \n";
 
 	for(auto &c : matrix){
-		for(int j = 0; j < *M; ++j){
-			std::cin >> *temp;
+		for(int j = 0; j < M; ++j){
+			std::cin >> temp;
 			//s >> *temp;
-			c.push_back(*temp);
+			c.push_back(temp);
 		}
 	}
 
 
-	delete N;
-	delete M;
-
-	unsigned int* size_e = new unsigned int(matrix[0].size());
-	unsigned int* size_c = new unsigned int(matrix.size());
+	unsigned int size_e = matrix[0].size();
+	unsigned int size_c = matrix.size();
 
 	//vector with numbers of matrix's columns in which there are no diagonal element even after swaping the rows
 	std::vector<unsigned int> zero_columns;
 
 	auto k = 0U;
-	for(auto i = 0U; k < *size_e - 1 && i < *size_c; ++i, ++k, ++row){
+	for(auto i = 0U; k < size_e - 1 && i < size_c; ++i, ++k, ++row){
 		//check diagonal elemnts
 		if(is_equal(matrix[i][k], 0.0)){
 			auto j = 0U;
 			zero_columns.empty() ? j = i + 1U : j = zero_columns[0];
-			for(; j < *size_c && is_equal(matrix[j][k], 0.0); ++j);
+			for(; j < size_c && is_equal(matrix[j][k], 0.0); ++j);
 
-			if(j == *size_c){
+			if(j == size_c){
 				zero_columns.push_back(k);
 				--i;
 				--row;
@@ -151,25 +148,25 @@ int main(){
 			std::swap(matrix[i], matrix[j]);
 		}
 		double t = matrix[i][k];
-		for(auto m = 0U; m < *size_e; ++m){
+		for(auto m = 0U; m < size_e; ++m){
 			matrix[i][m] /= t;
 		}
 
 
-		for(auto j = 0U; j < *size_c; ++j){
+		for(auto j = 0U; j < size_c; ++j){
 			if(j == i){
 				continue;
 			}
 			double t = matrix[j][k];
-			for(auto m = 0U; m < *size_e; ++m){
+			for(auto m = 0U; m < size_e; ++m){
 				matrix[j][m] -= t*matrix[i][m];
 			}
 		}
 	}
 
 	//if we got the diagonal elements that are in matrix, but after last catched column whith one of them there are columns that must be counted
-	if(k < *size_e){
-		while(k != *size_e - 1){
+	if(k < size_e){
+		while(k != size_e - 1){
 			zero_columns.push_back(k);
 			++k;
 		}
@@ -183,7 +180,7 @@ int main(){
 	std::cout << '\n';
 
 	
-	if(row < *size_c){
+	if(row < size_c){
 		std::cout << "the system is incompatible";
 		return 0;
 	}
