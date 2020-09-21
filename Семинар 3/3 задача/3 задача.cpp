77e350e5019df69817a88f3e8b53ef39299d984c
NeBoris <69312516+NeBoris@users.noreply.github.com>
2020-09-21 19:19:23 +0300

    finished

---
+++ Семинар 3/3 задача/3 задача.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <functional>
+
+double calculate(double x, double y, const std::function<double(double, double)>& f) {
+	return f(x, y);
+}
+
+int main() {
+	std::vector<std::function<double(double, double)>> v;
+
+	v.push_back([](double x, double y) {return x + y; });
+	v.push_back([](double x, double y) {return x * y; });
+	v.push_back([](double x, double y) {return x / y; });
+	v.push_back([](double x, double y) {return pow(x, y); });
+
+	for (auto& c : v) {
+		double result = calculate(4, 2, c);
+		std::cout << result << "\n";
+
+	}
+	
+	return EXIT_SUCCESS;
+}
