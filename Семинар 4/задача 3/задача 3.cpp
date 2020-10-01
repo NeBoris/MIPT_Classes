f295d058ae5942005f594aa7b4191b78b5707d38
NeBoris <69312516+NeBoris@users.noreply.github.com>
2020-10-01 19:29:01 +0300

    last one

--- Семинар 4/задача 3/задача 3.cpp
+++ Семинар 4/задача 3/задача 3.cpp
@@ -2,8 +2,13 @@
 #include <string>
 #include <sstream>
 
+
+
 struct time {
-	time(const int new_hours, const int new_minutes, const int new_seconds, const int new_milliseconds) :
+	time(const int new_year, const int new_month, const int new_day, const int new_hours, const int new_minutes, const int new_seconds, const int new_milliseconds) :
+		year(new_year + new_month / 12 + new_day / 31 + new_hours % 24 + new_minutes / 60 + new_seconds / 60 + new_milliseconds / 1000),
+		month(new_month % 12 + new_day / 31 + new_hours % 24 + new_minutes / 60 + new_seconds / 60 + new_milliseconds / 1000),
+		day(new_day % 31 + new_hours % 24 + new_minutes / 60 + new_seconds / 60 + new_milliseconds / 1000),
 		hours(new_hours % 24 + new_minutes / 60 + new_seconds / 60 + new_milliseconds / 1000),
 		minutes(new_minutes % 60 + new_seconds / 60 + new_milliseconds / 1000),
 		seconds(new_seconds % 60 + new_milliseconds / 1000),
@@ -11,10 +16,13 @@ struct time {
 
 	std::string for_print() const {
 		std::stringstream result;
-		result << hours << ":" << minutes << ":" << seconds << ":" << milliseconds;
+		result << year << ":" << month << ":" << day << "     " << hours << ":" << minutes << ":" << seconds << ":" << milliseconds;
 		return result.str();
 	}
 
+	unsigned int year;
+	unsigned int month : 4;
+	unsigned int day : 5;
 	unsigned int hours : 5;
 	unsigned int minutes : 6;
 	unsigned int seconds : 6;
@@ -22,7 +30,7 @@ struct time {
 };
 
 int main() {
-	time new_time(13, 65, 14, 1025);
+	time new_time(2020, 10, 1, 13, 65, 14, 1025);
 
 	std::cout << new_time.for_print();
 
