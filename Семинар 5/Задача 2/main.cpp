1bc691b566c4297aa31befd17c404bbb5299e262
NeBoris <69312516+NeBoris@users.noreply.github.com>
2020-10-10 01:00:53 +0300

    Update main.cpp

--- Семинар 5/Задача 2/main.cpp
+++ Семинар 5/Задача 2/main.cpp
@@ -2,30 +2,30 @@
 #include <string>
 #include <vector>
 
-static int c = 3;
-static int power = 10;
+static int c_in_2power = 9;
+static int power = 11;
 
 enum class ed_izm {
 	sm,
 	Far
 };
 
-class capacitance {
+class Capacitance {
 public:
 
-	capacitance(double new_value, int i) : 
+	Capacitance(double new_value, int i) : 
 		value(new_value), x(static_cast< ed_izm > (i) ) {}
 
 	void convert_to_sgs() {
-		value *= pow(c, 2) * pow(10, power * 2 - 9);
+		value *= c_in_2power * power;
 		x = static_cast<ed_izm> (2);
 	}
 
 	void convert_to_si() {
-		value /= pow(c, 2) * pow(10, power * 2 - 9);
+		value /= c_in_2power * power;
 		x = static_cast<ed_izm> (1);
 	}
-	friend std::ostream& operator<< (std::ostream& stream, const capacitance& k) {
+	friend std::ostream& operator<< (std::ostream& stream, const Capacitance& k) {
 		stream << k.value << (k.x == static_cast<ed_izm> (1) ? " Far" : " sm");
 		return stream;
 	}
@@ -45,7 +45,7 @@ int main() {
 	std::cin >> a >> b;
 	
 	
-	capacitance c(a, b);
+	Capacitance c(a, b);
 
 	if (b == 2) {
 		c.convert_to_si();
